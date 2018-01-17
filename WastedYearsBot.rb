994457bd77bef67f3f0c43863f3eb7af45de31c8
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-17 16:01:42 -0600

    almost done

--- WastedYearsBot.rb
+++ WastedYearsBot.rb
@@ -1,11 +1,22 @@
 require 'Twitter'
 require_relative 'lyrics'
+require 'active_support/time'
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV.fetch('TWITTER_CONSUMER_KEY')
-  config.consumer_secret     = ENV.fetch('TWITTER_CONSUMER_SECRET')
-  config.access_token        = ENV.fetch('TWITTER_ACCESS_TOKEN')
-  config.access_token_secret = ENV.fetch('TWITTER_ACCESS_TOKEN_SECRET')
+def tweetRock
+  lyrics = Lyrics.class_variable_get(:@@lyricHash)
+  lyrics.each do |key, val|
+    client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV.fetch('TWITTER_CONSUMER_KEY')
+        config.consumer_secret     = ENV.fetch('TWITTER_CONSUMER_SECRET')
+        config.access_token        = ENV.fetch('TWITTER_ACCESS_TOKEN')
+        config.access_token_secret = ENV.fetch('TWITTER_ACCESS_TOKEN_SECRET')
+      end
+      t = Time.now
+      tweet = val
+      # client.update(tweet)
+      puts tweet
+      sleep (t + 1.second - Time.now)
+  end
 end
 
-client.update("I'm posting a tweet!")
+tweetRock
--- lyrics.rb
+++ lyrics.rb
@@ -1,26 +1,26 @@
-lyricHash = {
-    intro: "(Face-melting intro)\nBEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-BEEP-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-BEEP\nDRR-NRRRRRRRRRRRRRRRRRRRRR\nBOOM BLAT",
-    one: "From the coast of gold, across the seven seas",
-    two: "I'm traveling on, far and wide",
-    three: "But now it seems, I'm just a stranger to myself",
-    four: "And all the things I sometimes do, it isn't me but someone else",
-    five: "I close my eyes, and think of home",
-    six: "Another city goes by, in the night",
-    seven: "Ain't it funny how it is, you never miss it til it's gone away",
-    eight: "And my heart is lying there and will be til my dying day",
-    nine: "So understand",
-    ten: "Don't waste your time always searching for those wasted years",
-    eleven: "Face up, make your stand",
-    twelve: "And realize you're living in the golden years",
-    thirteen: "Too much time on my hands, I got you on my mind",
-    fourteen: "Can't ease this pain, so easily",
-    fifteen: "When you can't find the words to say, it's hard to make it through another day",
-    sixteen: "And it makes me want to cry, and throw my hands up to the sky",
-    seventeen: "So understand",
-    eighteen: "Don't waste your time always searching for those wasted years",
-    nineteen: "Face up, make your stand",
-    twenty: "And realize you're living in the golden years",
-    outro: "(Face-melting outro)\nBEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-BEEP-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-BEEP\nDRR-NRR-NRRRR-NRRRRRRRRRRRRRRRRRRRRR"
-}
-
-puts lyricHash
+class Lyrics
+    @@lyricHash = {
+        intro: "(Face-melting intro)\nBEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-BEEP-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-BEEP\nDRR-NRRRRRRRRRRRRRRRRRRRRR\nBOOM BLAT",
+        one: "From the coast of gold, across the seven seas",
+        two: "I'm traveling on, far and wide",
+        three: "But now it seems, I'm just a stranger to myself",
+        four: "And all the things I sometimes do, it isn't me but someone else",
+        five: "I close my eyes, and think of home",
+        six: "Another city goes by, in the night",
+        seven: "Ain't it funny how it is, you never miss it til it's gone away",
+        eight: "And my heart is lying there and will be til my dying day",
+        nine: "So understand",
+        ten: "Don't waste your time always searching for those wasted years",
+        eleven: "Face up, make your stand",
+        twelve: "And realize you're living in the golden years",
+        thirteen: "Too much time on my hands, I got you on my mind",
+        fourteen: "Can't ease this pain, so easily",
+        fifteen: "When you can't find the words to say, it's hard to make it through another day",
+        sixteen: "And it makes me want to cry, and throw my hands up to the sky",
+        seventeen: "So understand",
+        eighteen: "Don't waste your time always searching for those wasted years",
+        nineteen: "Face up, make your stand",
+        twenty: "And realize you're living in the golden years",
+        outro: "(Face-melting outro)\nBEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-BEEP-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-budu-BEEP-budu-BEEP\nDRR-NRR-NRRRR-NRRRRRRRRRRRRRRRRRRRRR"
+    }
+end
