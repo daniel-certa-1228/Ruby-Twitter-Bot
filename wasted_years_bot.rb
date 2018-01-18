53e07c3826251979c4021fc6d5355e4437fd1d05
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-18 14:16:38 -0600

    created counter database to persist counter value

--- wasted_years_bot.rb
+++ wasted_years_bot.rb
@@ -1,29 +1,47 @@
 require 'twitter'
+require 'pg'
 require_relative 'lyrics'
 
-lyrics = Lyrics.class_variable_get(:@@lyricHash)
-keys = lyrics.keys
-file = File.open('counter.txt', 'r+')
-count = file.read.to_i
-tweet = lyrics[keys[count]]
+begin
 
-client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV.fetch('TWITTER_CONSUMER_KEY')
-      config.consumer_secret     = ENV.fetch('TWITTER_CONSUMER_SECRET')
-      config.access_token        = ENV.fetch('TWITTER_ACCESS_TOKEN')
-      config.access_token_secret = ENV.fetch('TWITTER_ACCESS_TOKEN_SECRET')
+  lyrics = Lyrics.class_variable_get(:@@lyricHash)
+  keys = lyrics.keys
+  c = PG.connect( dbname: 'wy_counter')
+  db_num = c.exec( "SELECT counter FROM counter;").getvalue 0,0
+  count = db_num.to_i
+
+  tweet = lyrics[keys[count]]
+
+  client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV.fetch('TWITTER_CONSUMER_KEY')
+        config.consumer_secret     = ENV.fetch('TWITTER_CONSUMER_SECRET')
+        config.access_token        = ENV.fetch('TWITTER_ACCESS_TOKEN')
+        config.access_token_secret = ENV.fetch('TWITTER_ACCESS_TOKEN_SECRET')
+    end
+    
+  client.update(tweet)
+
+  count += 1
+
+  if count == lyrics.length
+    c.exec( "UPDATE counter SET counter = 0;")  
+  else
+    c.exec( "UPDATE counter SET counter = #{count};")
   end
-  
-client.update(tweet)
 
-count += 1
+  puts count
 
-file.rewind
-
-if count == lyrics.length
-  file.write('0 ')
-else
-  file.write(count)
+rescue PG::Error => e
+  puts e.message
+ensure
+  c.close if c
 end
 
-file.close
+# file.rewind
+
+#   file.write('0 ')
+# else
+#   file.write(count)
+# end
+
+# file.close
