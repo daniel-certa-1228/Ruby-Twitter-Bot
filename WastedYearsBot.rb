911e50f152a79c4d6d88054f5ae9c5271fefd413
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-17 20:32:20 -0600

    ready for next test deploy

--- WastedYearsBot.rb
+++ WastedYearsBot.rb
@@ -1,21 +1,29 @@
 require 'Twitter'
 require_relative 'lyrics'
-require 'active_support/time'
 
-def tweetRock
-  lyrics = Lyrics.class_variable_get(:@@lyricHash)
-  lyrics.each do |key, val|
-    client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = ENV.fetch('TWITTER_CONSUMER_KEY')
-        config.consumer_secret     = ENV.fetch('TWITTER_CONSUMER_SECRET')
-        config.access_token        = ENV.fetch('TWITTER_ACCESS_TOKEN')
-        config.access_token_secret = ENV.fetch('TWITTER_ACCESS_TOKEN_SECRET')
-      end
-      t = Time.now
-      tweet = val
-      client.update(tweet)
-      sleep (t + 1.hour - Time.now)
+lyrics = Lyrics.class_variable_get(:@@lyricHash)
+keys = lyrics.keys
+file = File.open('counter.txt', 'r+')
+count = file.read.to_i
+tweet = lyrics[keys[count]]
+
+client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV.fetch('TWITTER_CONSUMER_KEY')
+      config.consumer_secret     = ENV.fetch('TWITTER_CONSUMER_SECRET')
+      config.access_token        = ENV.fetch('TWITTER_ACCESS_TOKEN')
+      config.access_token_secret = ENV.fetch('TWITTER_ACCESS_TOKEN_SECRET')
   end
+  
+client.update(tweet)
+
+count += 1
+
+file.rewind
+
+if count == lyrics.length
+  file.write('0 ')
+else
+  file.write(count)
 end
 
-tweetRock
+file.close
