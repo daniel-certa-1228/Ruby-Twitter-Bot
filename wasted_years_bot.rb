1b4b4a19ff8a9028e4a95b02db521de858160c7b
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-19 10:17:38 -0600

    fixed bug where twitter blocked repeat tweets

--- wasted_years_bot.rb
+++ wasted_years_bot.rb
@@ -11,18 +11,18 @@ begin
   c = PG.connect( uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password )
   db_num = c.exec( "SELECT counter FROM counter;").getvalue 0,0
   count = db_num.to_i #The current count
-
   tweet = lyrics[keys[count]] #defines the tweet as the value that is at the index number of the count
-
+  
   client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = ENV.fetch('TWITTER_CONSUMER_KEY')
-        config.consumer_secret     = ENV.fetch('TWITTER_CONSUMER_SECRET')
-        config.access_token        = ENV.fetch('TWITTER_ACCESS_TOKEN')
-        config.access_token_secret = ENV.fetch('TWITTER_ACCESS_TOKEN_SECRET')
-    end
-    
-  client.update(tweet) #sends the tweet
-
+          config.consumer_key        = ENV.fetch('TWITTER_CONSUMER_KEY')
+          config.consumer_secret     = ENV.fetch('TWITTER_CONSUMER_SECRET')
+          config.access_token        = ENV.fetch('TWITTER_ACCESS_TOKEN')
+          config.access_token_secret = ENV.fetch('TWITTER_ACCESS_TOKEN_SECRET')
+      end
+  
+  ret = client.update(tweet) #sends the tweet
+  puts ret.to_hash # inspect the return from twitter
+ 
   count += 1 #increment the counter
 
   if count == lyrics.length #if the count equals the length of the hash, it needs to be reset to zero to start at the beginning
@@ -32,6 +32,8 @@ begin
   end
   
 rescue PG::Error => e
+  puts e.message
+rescue Twitter::Error => e
   puts e.message
 ensure
   c.close if c #close the db connection
